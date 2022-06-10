b487b9a52209bd93e7ac89f1a70dc98e3aea5e9a
LMakuc <luka.makuc04@gmail.com>
2022-06-10 14:41:02 +0200

    Android app

---
+++ android/app/src/main/java/com/razum/fitnes/MainActivity.java
@@ -0,0 +1,5 @@
+package com.razum.fitnes;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.razum.fitnes',
+  appName: 'Fitnes App',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
